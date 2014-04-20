de95dacc1bbc47c5bcba2a3f8022d2feb38173dc
Marcelo Busico <marcelo.busico@mercadolibre.com>
2014-04-20 18:04:08 -0300

    Ejercicio 1

---
+++ AndroidTraining-Busico-Ejercicio1/ejercicio1/src/main/java/com/busico/training/ejercicio1/MainActivity.java
@@ -0,0 +1,48 @@
+package com.busico.training.ejercicio1;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private Button btnSearch;
+    private SearchView searchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        searchView = (SearchView) findViewById(R.id.searchView);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchProduct();
+            }
+        });
+    }
+
+    private void searchProduct() {
+        showSearchResult(getString(R.string.searchSuccessful, searchView.getQuery().toString()));
+    }
+
+    private void showSearchResult(String content) {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(getString(R.string.dialogTitle));
+        alertDialog.setMessage(content);
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.btnOk),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+        alertDialog.setIcon(R.drawable.ic_launcher);
+        alertDialog.show();
+    }
+}
